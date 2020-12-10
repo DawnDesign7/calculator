c447c7750571573f168ed50dfb3492845f4c2709
DawnDesign7 <dawnmoraa@gmail.com>
2020-12-10 16:42:11 +0300

    add division, multiplication, subtraction and remainder of functions

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,31 @@ var result = subtraction(number1, number2);
 alert(result);
 
 //multiplication
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
 
 //division
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
 
 //remainder of
+var remainder = function(number1, number2) {
+  return number1 % number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = remainder(number1, number2);
+alert(result);
