7c8e66dba022727782943fac07541cc990c270f2
DawnDesign7 <dawnmoraa@gmail.com>
2020-12-10 16:37:21 +0300

    add adittion and subtraction functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//addition
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -6,3 +7,21 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+//subtraction
+var subtraction = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtraction(number1, number2);
+alert(result);
+
+//multiplication
+
+
+//division
+
+
+//remainder of
